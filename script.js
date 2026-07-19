486723b282f241323af1e03cb5933ef64f9ab845
Vladyslav Radchenko <proffesional.email10@gmail.com>
2026-07-19 11:20:40 +0300

    Add Nunito font, implement clear button and improve interface

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const consumptionInput = document.getElementById("consumption");
 const fuelInput = document.getElementById("fuel");
 const distanceInput = document.getElementById("distance");
 const calculateButton = document.getElementById("calculate");
+const clearButton = document.getElementById("reset");
 
 calculateButton.addEventListener("click", function (){
+
+    if (
+        consumptionInput.value === "" ||
+        fuelInput.value === "" ||
+        distanceInput.value === ""
+    ) {
+        alert("Заповність усі поля!");
+        return;
+    }
+
+
     const consumption = Number(consumptionInput.value);
     const fuel = Number(fuelInput.value);
     const distance = Number(distanceInput.value);
@@ -12,4 +24,11 @@ calculateButton.addEventListener("click", function (){
 
     const result = document.getElementById("result");
     result.textContent = `Ви витратите ${fuelUsed} л. палива`;
+});
+
+clearButton.addEventListener("click",
+function (){
+    consumptionInput.value = "";
+    fuelInput.value = "";
+    distanceInput.value = "";
 });
